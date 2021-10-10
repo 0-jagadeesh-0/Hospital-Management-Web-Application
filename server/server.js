acb58c6a58dbf3bac19c340e6d320e3e47f84139
Jagadeesh <74336885+0-jagadeesh-0@users.noreply.github.com>
2021-10-10 18:31:16 +0530

    adding ejs

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const express=require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
+const ejs = require("ejs");
+
+
 
 // Server 
 
@@ -8,13 +11,36 @@ const app = express();
 
 // Middlewares 
 
-const static_path=path.join(__dirname,"../client/pages/home.html");
+const static_path=path.join(__dirname,"../client");
+
+app.set("view engine","ejs");
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(static_path));
 
-app.get("/",function (req,res) {
-    res.send("hello");
+
+app.get("/",(req,res)=> {
+    res.render("home");
 })
+
+app.get("/doctorinfo",(req,res)=>{
+    res.render("doctorinfo");
+})
+app.get("/doctorinfo",(req,res)=>{
+    res.render("doctorinfo");
+})
+
+app.get("/support",(req,res)=>{
+    res.render("support");
+})
+
+app.get("/contact",(req,res)=>{
+    res.render("contact");
+})
+
+
 app.listen(3000,function() {
     console.log("Server running");
 })
